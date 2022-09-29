f425c4c36dac94758a36c4e111d18d9ea3002c17
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-09-29 15:52:23 +0200

    create basic html for header navbar and card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <div>test</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>React and Morty App</header>
+            <article>
+                <img
+                    src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+                    alt="Morty Smith"
+                ></img>
+                <section>
+                    <h2>Morty Smith</h2>
+                    <button>Show more</button>
+                </section>
+            </article>
+            <nav>
+                <div>Home</div>
+                <div>Random</div>
+                <div>Favorites</div>
+                <div>Get creative</div>
+            </nav>
+        </div>
+    )
 }
 
-export default App;
+export default App
